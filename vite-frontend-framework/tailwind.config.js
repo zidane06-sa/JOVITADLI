e14ba97f6a4fde5344765f999bc24891cb35cd64
idan <113411952+zidane06-sa@users.noreply.github.com>
2025-12-15 03:26:50 +0700

    fix: define tailwind theme colors for css variables

--- vite-frontend-framework/tailwind.config.js
+++ vite-frontend-framework/tailwind.config.js
@@ -1,10 +1,17 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
